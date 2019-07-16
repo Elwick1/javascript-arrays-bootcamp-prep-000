4dea8a3ee10076c56ecd74393a4a0449b83d1b0b
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 21:41:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function accessElementInArray(array, index) {
   var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
-  return ('accessElementInArray')
+  console.log(accessElementInArray)
 }
