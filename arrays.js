b7ee7cb03c3be5a46d76ccbdf945e1a2d291c5ae
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 22:01:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,6 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
-  var chocolateBars = ...
   return ["foo", + 1]
 }
 
