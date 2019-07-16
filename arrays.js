dd4d399f9927182e1bd0c045f5de482c53021dd8
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 21:23:07 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
+  return ('addElementToBeginningOfArray')
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
+  return ["foo", + 1]
+}
