afbe89e38622f1b6a009534d260e1c96ff5e6b23
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 23:12:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(-1)
-  return array.slice(-1)
+  array.slice(1)
+  return array.slice(1)
   }
   
   function destructivelyRemoveElementFromEndOfArray(array) {
