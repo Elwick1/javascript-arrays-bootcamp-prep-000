0642d0170f3654df4fb11cfb3322b9b20b23960f
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 22:39:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,7 @@ function addElementToEndOfArray(array, element) {
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.unshift(element)
+  push.
   return ('destructivelyAddElementToEndOfArray', [1, "foo"])
 }
 
