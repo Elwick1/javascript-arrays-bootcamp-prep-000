74b22a44e14caea905819ae4c6692bb71a3593ab
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 21:58:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,9 @@ function accessElementInArray(array, index) {
   console.log(accessElementInArray[3])
   return (3)
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  var chocolateBars = ["snickers", "hundred grand", "kitkat" , "skittles"]
+  chocolateBars.shift(3)
+  return (2, 3)
+}
