aab32390788dd1017aa94abde24a87fef7e7340b
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-07-16 22:41:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToEndOfArray(array, element) {
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.unshift(element)
-  chocolateBars.push(element)
+  .push(element)
   return ('destructivelyAddElementToEndOfArray', [1, "foo"])
 }
 
